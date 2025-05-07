50dec0e60d9b6831d68bd0da7a809bbb57ba5a04
Erojaspintos <erojaspintos@gmail.com>
2025-05-07 15:51:35 -0300

    cambiamos logs por sentry.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const Sentry = require("@sentry/node");
+require("./utils/instrument");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -6,27 +8,29 @@ const app = express();
 const authRouter = require("./routes/auth.router");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
-
-//const loggerMiddleWare = require("./middlewares/logger.middleware");
+const loggerMiddleWare = require("./middlewares/logger.middleware");
 const authMiddleWare = require("./middlewares/auth.middleware");
 const sanitizerMiddleware = require("./middlewares/sanitizer.middleware");
 const privateRouter = require("./routes/private.router");
 const publicRouter = require("./routes/public.router");
 const connectMongoDB = require("./models/schemas/mongo.client");
+const logger = require("./utils/logger.js");
 
 (async () => {
   try {
     await connectMongoDB();
+    logger.info("Conexión a MongoDB establecida correctamente");
   } catch (error) {
-    console.log("Ha ocurrido un erro al intentar conectarse a MONGODB: ", error);
+    logger.sentryError({
+      message: "Ha ocurrido un error al intentar conectarse a MongoDB: ",
+      error,
+    });
     process.exit(1);
   }
 })();
-
- 
 // Middlewares
 app.use(express.json());
-//app.use(loggerMiddleWare);
+app.use(loggerMiddleWare);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(sanitizerMiddleware);
@@ -38,10 +42,14 @@ app.use("/v1/auth", authRouter);
 // Ruta publica para la documentacion : http://localhost:3001/documentation/#/
 app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // 
 
+app.get("/sentry-test", (req, res) => {
+  throw new Error("Error de prueba para Sentry");
+});
 app.use(authMiddleWare);
 // Private
 app.use("/v1", privateRouter);
 
-const PORT = process.env.PORT;
+Sentry.setupExpressErrorHandler(app);
+
 
 module.exports = app;
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,23 @@
-const path = require("path");
- 
-const logRequest = (req) => {
- 
-  const now = new Date().toISOString();
-  console.log(`[${now}] METHOD: ${req.method} ${req.url}`);
- 
+const winston = require("winston");
+const Sentry = require("./instrument");
+
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+// Captura manual de errores en Sentry + log
+logger.sentryError = (err) => {
+  Sentry.captureException(err);
+  logger.error(err.stack || err.message);
 };
- 
-module.exports = logRequest;
+
+module.exports = logger;
+
